5cf2a41dc2bbb9d28cbf8dd32a5377369c9ecd9a
JamesYang <gn00999829@gmail.com>
2020-01-30 16:32:00 -0800

    first push

---
+++ controllers/controller.js
@@ -0,0 +1,117 @@
+const Riddle = require("../models/riddle");
+const Comment = require("../models/comment");
+const util = require("../util/util");
+const url = require("url");
+
+exports.getHomePage = (req, res, next) => {
+  const filter = req.query.filter;
+  Riddle.getAll().then(async riddles => {
+    // Filter the riddles here //
+    let sortedRiddles = await util.filterRiddle(filter, riddles);
+    res.render("home", { riddles: sortedRiddles, filter });
+  });
+};
+
+exports.createRiddle = (req, res, next) => {
+  res.render("create");
+};
+
+exports.postRiddle = (req, res, next) => {
+  const riddle = new Riddle(
+    req.body.author,
+    req.body.title,
+    req.body.content,
+    util.getRandomBgImg()
+  );
+  riddle.saveRiddle();
+  res.redirect("/");
+};
+
+exports.detailRiddle = (req, res, next) => {
+  const riddleId = req.params.riddleId;
+  const isEditing = req.query.edit;
+
+  Riddle.getOne(riddleId).then(riddle => {
+    riddle.date = util.getFormattedDate(riddle.date);
+    const bgImgFile = riddle.image_url || util.getRandomBgImg();
+    Comment.getAllComment().then(comments => {
+      res.render("detail", {
+        riddle,
+        comments,
+        editMode: isEditing,
+        bgImgFile,
+        editComment: req.query.editComment,
+        editCommentId: req.query.editCommentId,
+        createComment: req.query.createComment
+      });
+    });
+  });
+};
+
+exports.deleteRiddle = async (req, res, next) => {
+  const riddleId = req.body.riddleId;
+  Comment.deleteAllComment(riddleId);
+  await Riddle.deleteRiddle(riddleId);
+  res.redirect("/");
+};
+
+exports.like = async (req, res, next) => {
+  const riddleId = req.body.riddleId;
+  const imgUrl = req.body.imgUrl;
+  await Riddle.like(riddleId);
+  res.redirect("/riddles/" + riddleId + "?imgUrl=" + imgUrl);
+};
+
+exports.showCommentForm = (req, res, next) => {
+  res.redirect(
+    url.format({
+      pathname: "/riddles/" + req.query.riddleId,
+      query: {
+        createComment: true
+      }
+    })
+  );
+};
+
+exports.createComment = (req, res, next) => {
+  const comment = new Comment(
+    req.body.riddleId,
+    req.body.author,
+    req.body.comment,
+    0,
+  );
+  comment.saveComment();
+  res.redirect("/riddles/" + req.body.riddleId);
+};
+
+exports.commentVote = async (req, res, next) => {
+  const { id, vote, riddle_id } = req.body;
+  let value = vote == 'agree'? 1: -1;
+  await Comment.voteComment(id, value)
+  res.redirect(`/riddles/${riddle_id}`)
+}
+exports.editComment = (req, res, next) => {
+  res.redirect(
+    url.format({
+      pathname: "/riddles/" + req.query.riddleId,
+      query: {
+        editComment: true,
+        editCommentId: req.query.commentId
+      }
+    })
+  );
+};
+
+exports.updateComment = async (req, res, next) => {
+  await Comment.updateComment(
+    req.body.commentId,
+    req.body.author,
+    req.body.comment
+  );
+  res.redirect("/riddles/" + req.body.riddleId);
+};
+
+exports.deleteComment = async (req, res, next) => {
+  await Comment.deleteComment(req.body.commentId);
+  res.redirect("/riddles/" + req.body.riddleId);
+};
---
+++ routes/route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const controller = require('../controllers/controller');
+const router = express.Router();
+
+router.get('/', controller.getHomePage);
+
+router.get('/create', controller.createRiddle);
+router.post('/create', controller.postRiddle);
+
+router.get('/riddles/:riddleId', controller.detailRiddle);
+router.post('/like', controller.like);
+router.post('/delete', controller.deleteRiddle);
+
+router.get('/comment/create', controller.showCommentForm);
+router.post('/comment/create', controller.createComment);
+router.post('/comment/vote', controller.commentVote);
+
+router.get('/comment/edit', controller.editComment);
+router.post('/comment/update', controller.updateComment);
+router.post('/comment/delete', controller.deleteComment);
+
+module.exports = router;
